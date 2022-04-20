34e9af355f78318d3539b21ff61e9b909510cdf9
AmyYangYuJia <86224961+AmyYangYuJia@users.noreply.github.com>
2022-04-20 09:38:14 +0800

    Update suanli.py

--- suanli.py
+++ suanli.py
@@ -4,10 +4,16 @@ def init(params: dict):
 
 
 def run(data: dict, func=None) -> bool:
-    if data["objectPath"] == "18013081_8796215827360_82705_81355_81355.jpg":
-        return True
-    return False
-
+#     if data["objectPath"] == "18013081_8796215827360_82705_81355_81355.jpg":
+#         return True
+#     return False
+      try:
+         raise FError('yichang')
+      expect FError as e:
+         print(e)
+         
+            
+           
 
 def teardown():
     pass
